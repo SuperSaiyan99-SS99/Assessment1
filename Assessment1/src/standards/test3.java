e84d32cde8e6b01dc5c78048d34645e1ebd0b05b
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-09-01 08:29:37 -0500

    Write programs for test

---
+++ Assessment1/src/standards/test3.java
@@ -0,0 +1,13 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//JOptionPane.showMessageDialog(null, "Hello");
+		System.out.println("Hi");
+	}
+
+}
